1b312bea3ce88bc8d7bb11f128b1e77015766256
Dmitry Naumov <dimon-naumov@yandex.ru>
2012-08-29 16:30:12 +0400

    Implemented "dead end" version

---
+++ CreateDatabase.sql
@@ -0,0 +1,24 @@
+﻿CREATE DATABASE [Eurovision]
+GO
+
+USE [Eurovision]
+GO
+
+CREATE TABLE [dbo].[VotingResults](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+	[Votes] [int] NOT NULL DEFAULT(0),
+	CONSTRAINT [PK_VotingResults] PRIMARY KEY CLUSTERED ([Id] ASC))
+GO
+
+INSERT INTO [dbo].[VotingResults] (Id, Name) VALUES (1, 'Great Britain')
+INSERT INTO [dbo].[VotingResults] (Id, Name) VALUES (2, 'Germany')
+INSERT INTO [dbo].[VotingResults] (Id, Name) VALUES (3, 'Italy')
+INSERT INTO [dbo].[VotingResults] (Id, Name) VALUES (4, 'Canada')
+INSERT INTO [dbo].[VotingResults] (Id, Name) VALUES (5, 'Russia')
+INSERT INTO [dbo].[VotingResults] (Id, Name) VALUES (6, 'USA')
+INSERT INTO [dbo].[VotingResults] (Id, Name) VALUES (7, 'France')
+INSERT INTO [dbo].[VotingResults] (Id, Name) VALUES (8, 'Japan')
+GO
+
+
--- WebApplication/Controllers/VotesController.cs
+++ WebApplication/Controllers/VotesController.cs
@@ -1,34 +1,58 @@
 ﻿namespace WebApplication.Controllers
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Configuration;
+	using System.Data.SqlClient;
 	using System.Threading.Tasks;
 	using System.Web.Http;
 	using Models;
 
 	public class VotesController : ApiController
 	{
-		private static readonly VotingResult[] _votes =
-			{
-				new VotingResult {Id = 1, Name = "Great Britain", Votes = 0},
-				new VotingResult {Id = 1, Name = "Germany", Votes = 0},
-				new VotingResult {Id = 1, Name = "Italy", Votes = 0},
-				new VotingResult {Id = 1, Name = "Canada", Votes = 0},
-				new VotingResult {Id = 1, Name = "Russia", Votes = 0},
-				new VotingResult {Id = 1, Name = "USA", Votes = 0},
-				new VotingResult {Id = 1, Name = "France", Votes = 0},
-				new VotingResult {Id = 1, Name = "Japan", Votes = 0},
-			};
+		private static readonly VotingResult Empty = new VotingResult();
+		private static readonly string ConnectionString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
 
         // GET api/votes
-        public async Task<VotingResult[]> Get()
+		public async Task<VotingResult[]> Get()
         {
-	        await Task.Delay(2000);
+			using (var connection = new SqlConnection(ConnectionString))
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText = "SELECT Id, Name, Votes FROM VotingResults";
 
-	        foreach (var votingResult in _votes)
-	        {
-		        votingResult.Votes += 1;
-	        }
+				await connection.OpenAsync();
+				using (var reader = await command.ExecuteReaderAsync())
+				{
+					var results = new List<VotingResult>();
 
-	        return _votes;
+					while (await reader.ReadAsync())
+					{
+						results.Add(new VotingResult { Id = reader.GetInt32(0), Name = reader.GetString(1), Votes = reader.GetInt32(2) });
+					}
+
+					return results.ToArray();
+				}
+			}
         }
+
+		// GET api/votes/5
+		public async Task<VotingResult> Get(int id)
+		{
+			// HACK:
+			id = 1 + new Random().Next(8);
+
+			using (var connection = new SqlConnection(ConnectionString))
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText = "UPDATE VotingResults SET Votes = Votes + 1 WHERE Id = @Id";
+				command.Parameters.AddWithValue("@Id", id);
+
+				await connection.OpenAsync();
+				await command.ExecuteNonQueryAsync();
+
+				return Empty;
+			}
+		}
     }
 }
