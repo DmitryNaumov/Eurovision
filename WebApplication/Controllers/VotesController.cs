2bb524e01abd50683dea5d03fa59c3f63a2d50ea
Dmitry Naumov <dnaumov@paladyne.com>
2012-08-30 14:20:00 +0400

    Load votes on restart

--- WebApplication/Controllers/VotesController.cs
+++ WebApplication/Controllers/VotesController.cs
@@ -1,35 +1,21 @@
 ﻿namespace WebApplication.Controllers
 {
 	using System;
-	using System.Configuration;
-	using System.Data.SqlClient;
-	using System.Threading;
 	using System.Threading.Tasks;
-	using System.Web;
 	using System.Web.Http;
 	using Models;
+	using WebApplication.Services;
 
 	public class VotesController : ApiController
 	{
 		private static readonly VotingResult Empty = new VotingResult();
-		private static readonly string ConnectionString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
 
-		private static readonly VotingResult[] _votes =
-			{
-				new VotingResult {Id = 1, Name = "Great Britain", Votes = 0},
-				new VotingResult {Id = 1, Name = "Germany", Votes = 0},
-				new VotingResult {Id = 1, Name = "Italy", Votes = 0},
-				new VotingResult {Id = 1, Name = "Canada", Votes = 0},
-				new VotingResult {Id = 1, Name = "Russia", Votes = 0},
-				new VotingResult {Id = 1, Name = "USA", Votes = 0},
-				new VotingResult {Id = 1, Name = "France", Votes = 0},
-				new VotingResult {Id = 1, Name = "Japan", Votes = 0},
-			};
+		private static readonly Lazy<VotesRepository> _repository = new Lazy<VotesRepository>(() => new VotesRepository());
 
 		// GET api/votes
 		public VotingResult[] Get()
 		{
-			return _votes;
+			return _repository.Value.GetResults();
 		}
 
 		// GET api/votes/5
@@ -38,39 +24,9 @@
 			// HACK:
 			id = 1 + new Random().Next(8);
 
-			using (var connection = new SqlConnection(ConnectionString))
-			using (var command = connection.CreateCommand())
-			{
-				command.CommandText = "INSERT INTO Votes (Id) VALUES (@Id)";
-				command.Parameters.AddWithValue("@Id", id);
+			await _repository.Value.AddVote(id);
 
-				await connection.OpenAsync();
-
-				using (var transaction = connection.BeginTransaction())
-				{
-					command.Transaction = transaction;
-
-					await command.ExecuteNonQueryAsync();
-					
-					// simulate db activity & roundtrip
-					await Task.Delay(10);
-
-					transaction.Commit();
-				}
-
-				AddVote(id - 1);
-
-				return Empty;
-			}
-		}
-
-		private void AddVote(int id)
-		{
-			var votingResult = _votes[id];
-			lock (votingResult)
-			{
-				votingResult.Votes++;
-			}
+			return Empty;
 		}
 	}
 }
---
+++ WebApplication/Services/VotesRepository.cs
@@ -0,0 +1,87 @@
+﻿namespace WebApplication.Services
+{
+	using System.Configuration;
+	using System.Data.SqlClient;
+	using System.Threading.Tasks;
+	using WebApplication.Models;
+
+	internal sealed class VotesRepository
+	{
+		private static readonly string ConnectionString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+
+		private readonly VotingResult[] _votes =
+			{
+				new VotingResult {Id = 1, Name = "Great Britain", Votes = 0},
+				new VotingResult {Id = 1, Name = "Germany", Votes = 0},
+				new VotingResult {Id = 1, Name = "Italy", Votes = 0},
+				new VotingResult {Id = 1, Name = "Canada", Votes = 0},
+				new VotingResult {Id = 1, Name = "Russia", Votes = 0},
+				new VotingResult {Id = 1, Name = "USA", Votes = 0},
+				new VotingResult {Id = 1, Name = "France", Votes = 0},
+				new VotingResult {Id = 1, Name = "Japan", Votes = 0},
+			};
+
+		public VotesRepository()
+		{
+			// HACK:	
+
+			using (var connection = new SqlConnection(ConnectionString))
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText = "SELECT Id FROM Votes";
+
+				connection.Open();
+				using (var reader = command.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						var id = reader.GetInt32(0);
+						IncrementVote(id);
+					}
+				}
+			}
+		}
+
+		public VotingResult[] GetResults()
+		{
+			return _votes;
+		}
+
+		public async Task<int> AddVote(int id)
+		{
+			using (var connection = new SqlConnection(ConnectionString))
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText = "INSERT INTO Votes (Id) VALUES (@Id)";
+				command.Parameters.AddWithValue("@Id", id);
+
+				await connection.OpenAsync();
+
+				using (var transaction = connection.BeginTransaction())
+				{
+					command.Transaction = transaction;
+
+					await command.ExecuteNonQueryAsync();
+
+					// simulate db activity & roundtrip
+					await Task.Delay(10);
+
+					transaction.Commit();
+				}
+
+				IncrementVote(id);
+
+				return 42;
+			}
+		}
+
+		private void IncrementVote(int id)
+		{
+			var votingResult = _votes[id - 1];
+			lock (votingResult)
+			{
+				votingResult.Votes++;
+			}
+		}
+	}
+}
