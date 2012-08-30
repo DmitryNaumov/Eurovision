7f5ede836d2bb18dcc5e672ee1e45626926dc85f
Dmitry Naumov <dnaumov@paladyne.com>
2012-08-30 13:18:35 +0400

    Use Mike SQL for demo

--- WebApplication/Scripts/Site.js
+++ WebApplication/Scripts/Site.js
@@ -20,7 +20,7 @@ function refresh() {
 
             setTimeout(
                 'refresh()',
-                '2000');
+                '1000');
         },
         error: function () {
             setTimeout(
